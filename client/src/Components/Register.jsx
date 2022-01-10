439d16886293d24345dbb3fadf13e5d0cc1cc025
David Nevarez <david.nevarez03@hotmail.com>
2022-01-09 19:55:01 -0700

    made it so users can login to ther account

--- client/src/Components/Register.jsx
+++ client/src/Components/Register.jsx
@@ -1,15 +1,22 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 
 import axios from "axios";
 import Input from "./Input";
-import {FaApple, FaEnvelope, FaEye, FaFacebookF, FaGoogle, FaTimes} from "react-icons/fa";
+import {
+  FaApple,
+  FaEnvelope,
+  FaEye,
+  FaFacebookF,
+  FaGoogle,
+  FaTimes,
+} from "react-icons/fa";
 
-import '../styles/components/Register.scss'
+import "../styles/components/Register.scss";
 
-const token = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySUQiOiI2MWQwZWM3ODI5MmYzMjgwZDY2NzE1YTciLCJuYW1lIjp7ImZpcnN0TmFtZSI6IkZlcm5hbmRvIiwibWlkZGxlTmFtZSI6IkRhdmlkIiwibGFzdE5hbWUiOiJOZXZhcmV6In0sImlhdCI6MTY0MTU5MjQ3OCwiZXhwIjoxNjQ0MTg0NDc4fQ.ZztCiKHlAU5LSJZYK7EVPa9qwlgyMNrUwyJnZiA06Lw`;
+const token = localStorage.getItem("userToken");
 
-const Register = ({close, change}) => {
-  const [error, setError] = useState("")
+const Register = ({ close, change }) => {
+  const [error, setError] = useState("");
 
   async function registerUser() {
     const response = await axios.post(
@@ -36,19 +43,19 @@ const Register = ({close, change}) => {
   // IF YOU NEED HELP WITH THIS CONTACT ETHAN
   const formSubmit = (e) => {
     e.preventDefault();
-    const {target} = e;
+    const { target } = e;
 
     // setError("some error triggered when form validation is failed")
     // To chnage styles
 
     // Do whatever you need with the form data
-  }
+  };
 
   // The valid provider names are "google", "facebook", or "apple"
   // Feel free to disassemble and reassemble however you'd like
   const provider = (name) => {
     // Do whatever you need to do with the providers
-  }
+  };
 
   return (
     <div className="register-popup">
@@ -69,14 +76,16 @@ const Register = ({close, change}) => {
               <Input name="fname" title="First Name" placeholder="John" />
               <Input name="lname" title="Last Name" placeholder="Doe" />
             </div>
-            <Input name="email" placeholder="example@gmail.com" icon={<FaEnvelope />} />
+            <Input
+              name="email"
+              placeholder="example@gmail.com"
+              icon={<FaEnvelope />}
+            />
             <Input name="password" type="password" icon={<FaEye />} />
             <Input name="confirm" type="password" icon={<FaEye />} />
           </div>
 
-          <button type="submit">
-            Submit
-          </button>
+          <button type="submit">Submit</button>
 
           <div className="sep">
             <div className="dash"></div>
@@ -101,7 +110,7 @@ const Register = ({close, change}) => {
         </form>
       </div>
     </div>
-  )
+  );
 
   // return (
   //   // user Register form

8189fcccd327685dfa6a0044b07e09c074f98e35
David Nevarez <david.nevarez03@hotmail.com>
2022-01-23 09:55:35 -0700

    updated error page

--- client/src/Pages/Error.jsx
+++ client/src/Pages/Error.jsx
@@ -1,7 +1,17 @@
 import React, { useState, useEffect } from "react";
+import Navbar from "../Components/Navbar";
+import "../styles/pages/Error.scss";
 
 const Error = () => {
-  return <div>404 Error</div>;
+  return (
+    <>
+      <Navbar />
+      <div className="error-container">
+        <h1>Uh Oh!</h1>
+        <p>This page seems to not exist</p>
+      </div>
+    </>
+  );
 };
 
 export default Error;

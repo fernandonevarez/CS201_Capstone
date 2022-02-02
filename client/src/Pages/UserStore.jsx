7b4defa2474ed9e19838cd82b7574c73508ab86f
David Nevarez <david.nevarez03@hotmail.com>
2022-02-01 17:25:23 -0700

    commiting for ethan

--- client/src/Pages/UserStore.jsx
+++ client/src/Pages/UserStore.jsx
@@ -2,12 +2,35 @@
 
 import React, {useState, useeffect} from 'react';
 import { useParams } from 'react-router-dom';
+import {useUser} from '../contexts/useUser';
 
 const UserStore = () => {
 
   const {userID} = useParams();
 
-  return <div>{userID}</div>;
+  const {user} = useUser();
+
+  const createStore = async () => {
+    
+  }
+
+  return (
+    <main>
+      <h1>User's Store</h1>
+      {
+        user.details.user.hasStore ? (
+          <div><h2>{user.details.user.storeName}</h2></div>
+        ) : (
+          
+            <div>
+              <h2>You don't have a store yet!</h2>
+            <button onClick={() => createStore()}>Create a store</button>
+            </div>
+            
+        )
+      }
+    </main>
+  );
 };
 
 export default UserStore;

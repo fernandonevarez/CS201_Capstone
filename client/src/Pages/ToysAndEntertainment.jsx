6a60ab9ef6ccf2ee61bfb9f7d673b80ee46a0d89
Alisson Corrales <Acorra435@west-mec.local>
2022-02-10 18:27:45 -0700

    working on styling

--- client/src/Pages/ToysAndEntertainment.jsx
+++ client/src/Pages/ToysAndEntertainment.jsx
@@ -1,11 +1,16 @@
-import React, {useState} from 'react';
-import Navbar from '../Components/Navbar';
+import React, { useState } from "react";
+import Navbar from "../Components/Navbar";
+import Footer from "../Components/Footer";
+import {} from "../styles/pages/ToysAndEntertainment.scss";
 
 const ToysAndEntertainment = () => {
   return (
-    <div className="page-conatiner">
-      <h1>Toys and Entertainment</h1>
+    <div className="page-container">
+      <div className="products-container">
+        <h2>Toys and Entertainment</h2>
       </div>
+      <Footer />
+    </div>
   );
 };
 

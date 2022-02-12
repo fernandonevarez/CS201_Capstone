9629f321e3280b8be4631351e6ddad0c9e089a0f
Alisson <alisson.corrales24@gmail.com>
2022-02-11 22:51:49 -0700

    working on stylings for the product page

--- client/src/Pages/SingleProduct.jsx
+++ client/src/Pages/SingleProduct.jsx
@@ -2,18 +2,21 @@ import React, { useState, useEffect } from "react";
 import Navbar from "../Components/Navbar";
 import axios from "axios";
 
+import "../styles/pages/SingleProduct.scss";
+
 import { useParams } from "react-router-dom";
 
 import { useUser } from "../contexts/useUser";
 
 // import { useAuth0 } from "@auth0/auth0-react";
+import duckImg from "../assets/images/temp/duck.jpg";
+import Footer from "../Components/Footer";
 
 let cancel;
 
 const token =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySUQiOiI2MWY1ZDdmMDU3YmQzYTQ1MWJjZmUyNjAiLCJuYW1lIjp7ImZpcnN0TmFtZSI6IlRpbSIsIm1pZGRsZU5hbWUiOiJLZXZpbiIsImxhc3ROYW1lIjoiUGhpbGwifSwiaWF0IjoxNjQzNTAxNjAwLCJleHAiOjE2NDYwOTM2MDB9.4sQiB07AI1GRc5Sp4AvE_5ds0zwe9AUo9yuQNBJN8A4";
 
-// const token = userUser.userCookies.token;
 
 const SingleProduct = () => {
   const [product, setProduct] = useState({});
@@ -113,6 +116,7 @@ const SingleProduct = () => {
     <main className="single-product-page">
       <div className="single-product-container">
         <div className="product-image-slide">
+          <img src={duckImg} alt="" />
           {/* {imageArray.map((image) => {
             return (
               <img
@@ -128,12 +132,21 @@ const SingleProduct = () => {
         <div className="product-info">
           <h1>{name}</h1>
           {/* <img src={imageArray[0]} alt="image of product" /> */}
-          <h2>&#65284;{price / 100}</h2>
+          
+          <div className="product-price-available">
+            <h2>&#65284;{price / 100}</h2>
+            <h2>In stock</h2>
+          </div>
+
+          <button onClick={() => pushToCart()}>Add to Cart</button>
+
+          <h2 className="details">Details</h2>
+
           <p>{description}</p>
         </div>
 
-        <button onClick={() => pushToCart()}>Add to Cart</button>
       </div>
+      <Footer />
     </main>
   );
 };

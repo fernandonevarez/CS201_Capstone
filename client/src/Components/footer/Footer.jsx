27028855acd17ed55a5662133dbb3287dc7f9e07
David Nevarez <david.nevarez03@hotmail.com>
2022-03-04 18:20:38 -0700

    working on the footer styling, got the base of the footer done

--- client/src/Components/footer/Footer.jsx
+++ client/src/Components/footer/Footer.jsx
@@ -1,91 +1,35 @@
-import React, { useState, useEffect, useMemo } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
-
-import {IoIosArrowUp, IoIosArrowDown} from 'react-icons/io';
 
 import "../../styles/components/footer/Footer.scss";
 
 import { useUser } from "../../contexts/useUser";
 import Dropdown from "./Dropdown";
 
-import useCollapse from "react-collapsed";
+
 
 const Footer = () => {
   const { user } = useUser();
 
-  const [showDropdown, setShowDropdown] = useState([{}]);
-
-  const dropdownSections = [
-    {
-      id: 1,
-      name: "Sell",
-      links: [
-        {
-          id: 1,
-          name: "Selling on MSB",
-          link: "/selling-info",
-          opatialRequirement: null,
-        },
-        {
-          id: 2,
-          name: "Start Selling",
-          // link: `/${user.details.user._id}/store`,
-          link: `/:userID/store`,
-          opatialRequirement: ["userAuth", "userStore"],
-        },
-      ],
-    },
-    {
-      id: 2,
-      name: "About",
-      links: [
-        {
-          id: 1,
-          name: "About MSB",
-          link: "/about",
-        },
-      ],
-    },
-  ];
-
-  // console.log("userID", user.details.user._id);
-
-  // const closeDropdown = (dropdownSectionID) => {
-  //   // get the dropdownsectionID and set the showDropdown to false
-  //   setShowDropdown(false);
-  // }
-
-  // const content = document.getElementById("footer-dropdown-content-1");
-  // if (showDropdown) {
-  //   content.style.display = "block";
-  //   // content.classList.remove("hide");
-  // } else {
-  //   // content.classList.remove("show");
-  //   // content.classList.add("hide");
-  //   content.style.display = "none";
-  // }
-  const { getCollapseProps, getToggleProps, isExpanded } = useCollapse();
-
+ 
   return (
     <div className="footer-container">
-      <Dropdown />
+      <Dropdown title="About" content={<Link to="/about">About MSB</Link>} />
 
-      <div className="collapsible">
-        <div className="header" {...getToggleProps()}>
-          <h3>Sell</h3>
-          {isExpanded ? <IoIosArrowUp/> : <IoIosArrowDown/>}  
-        </div>
-        <div className="collapsed-panel" {...getCollapseProps()}>
-          <div className="content">
-            <Link to="/selling-info">Selling on MSB</Link>
-            {user.details.isAuthenticated == true && user.details.user.hasStore == false ? (
-              <Link to={`/${user.details.user._id}/store`}>Start Selling</Link>
-            ) : null}
-
-
-          </div>
-        </div>
-      </div>
+      {user.details.isAuthenticated ? (
+        <Dropdown
+          title="Sell"
+          content={[
+            <Link to="/selling-info">Selling on MSB</Link>,
+            <Link to={`/${user.details.user._id}/store`}>Start selling</Link>,
+          ]}
+        />
+      ) : (
+        <Dropdown
+          title="Sell"
+          content={[<Link to="/selling-info">Selling on MSB</Link>]}
+        />
+      )}
     </div>
   );
 };
